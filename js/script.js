d34b9dd9d87674cd9b60a6234cc8024374153508
Nicky Crispino <crispinonicky@gmail.com>
2023-04-22 19:50:06 -0400

    Add full functionality for location checkboxes

--- js/script.js
+++ js/script.js
@@ -1,66 +1,79 @@
-//console.log("Script loaded")
-
 // Retrieve JSON chip data
 var chipData = window.chipData;
 
 // Loop through data to render page
 chipData.forEach((chip, i) => {
-    //console.log(chip)
     $("#main").append(`
         <div class="chip-row col-sm-12 row py-3 border-bottom">
             <div class="col-sm-2">${chip.name}</div>
             <div class="col-sm-4">${chip.description}</div>
-            <div class="chip-codes col-sm-6 d-flex"></div>
+            <div class="chip-codes col-sm-6 d-flex justify-content-start"></div>
         </div>
     `)
 
     chip.codes.split("").forEach((code, j) => {
         $($(".chip-codes")[i]).append(`
-            <div class="col-sm-2">
-                <div class="w-50 d-flex justify-content-between form-check">
-                    <label class="form-check-label" for="${chip.name}-${code}">${code}</label>
-                    <input class="chip-checkbox form-check-input" type="checkbox" name="${chip.name}-${code}" id="${chip.name}-${code}">
-                </div>
+            <div class="code-input form-check form-check-reverse col-sm-2">
+                <label class="form-check-label" for="${chip.name}-${code}">${code}</label>
+                <input class="chip-checkbox form-check-input" type="checkbox" name="${chip.name}-${code}" id="${chip.name}-${code}">
             </div>
         `)
     })
 })
 
 // Retrieve checkbox inputs on page load
-var checked = [];
+var checkedChips = [];
 
-// Save checkbox inputs
+// Save chip checkbox inputs
 $(".chip-checkbox").on("click", function() {
-    //console.log($(this).attr("name"))
-    //console.log($(this).is(':checked'))
 
     let chipName = $(this).attr("name");
     let isChecked = $(this).is(':checked');
 
     if (isChecked) {
-        checked.push(chipName)
+        checkedChips.push(chipName)
     } else {
-        checked.splice(checked.indexOf(chipName), 1);
+        checkedChips.splice(checked.indexOf(chipName), 1);
     }
 
-    console.log(checked)
-
-    // console.log(checked)
-    // console.log(checked.toString());
-
-    localStorage.setItem("checked", checked.toString())
+    localStorage.setItem("checked", checkedChips.toString())
     countChips();
 
 })
 
+// Check chip code checkboxes on page load
 if (localStorage.getItem("checked") != null && localStorage.getItem("checked") != []) {
-    checked = localStorage.getItem("checked").split(",");
-
-    console.log(checked)
-
-    checked.forEach(inputName => {
-        console.log(inputName)
+    checkedChips = localStorage.getItem("checked").split(",");
+    checkedChips.forEach(inputName => {
         $(`.chip-checkbox[name='${inputName}']`).prop("checked", true);
+    })
+}
+
+// Retrieve checkbox inputs on page load
+var checkedLocations = [];
+
+// Save location checkbox inputs
+$("#location-sidebar input").on("click", function() {
+
+    let locationName = $(this).attr("name");
+    let isChecked = $(this).is(':checked');
+
+    if (isChecked) {
+        checkedLocations.push(locationName)
+    } else {
+        checkedLocations.splice(checkedLocations.indexOf(locationName), 1);
+    }
+
+    localStorage.setItem("checkedLocations", checkedLocations.toString())
+    countChips();
+
+})
+
+// Check location checkboxes on page load
+if (localStorage.getItem("checkedLocations") != null && localStorage.getItem("checkedLocations") != []) {
+    checkedLocations = localStorage.getItem("checkedLocations").split(",");
+    checkedLocations.forEach(inputName => {
+        $(`#location-sidebar input[name='${inputName}']`).prop("checked", true);
     })
 }
 
@@ -83,5 +96,5 @@ countChips();
 function toggleLocations() {
     $("#location-sidebar").toggleClass("d-none");
     $(".chip-row").toggleClass("col-sm-12");
-    $(".chip-row").toggleClass("col-sm-8");
+    $(".chip-row").toggleClass("col-sm-9");
 }
